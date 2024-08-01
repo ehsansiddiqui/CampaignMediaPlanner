8c4ab10bbc60dee928c1698dc3159c505687df1f
ehsansiddiqui <ihsaansiddiqui@gmail.com>
2024-08-01 09:29:53 +0200

    Initial commit

---
+++ CampaignMediaPlanner.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CampaignMediaPlanner.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ CampaignMediaPlanner/Services/GoalSeekService.cs
@@ -0,0 +1,34 @@
+// Services/GoalSeekService.cs
+namespace CampaignMediaPlanner.Services
+{
+    using System;
+
+    public class GoalSeekService
+    {
+        public double GoalSeek(Func<double, double> equation, double target, double initialGuess, double tolerance = 0.0001, int maxIterations = 1000)
+        {
+            double guess = initialGuess;
+            double result = equation(guess);
+            int iterations = 0;
+
+            while (Math.Abs(result - target) > tolerance && iterations < maxIterations)
+            {
+                guess = guess - (result - target) / Derivative(equation, guess);
+                result = equation(guess);
+                iterations++;
+            }
+
+            if (iterations == maxIterations)
+            {
+                throw new Exception("Goal Seek did not converge.");
+            }
+
+            return guess;
+        }
+
+        private double Derivative(Func<double, double> equation, double x, double h = 0.0001)
+        {
+            return (equation(x + h) - equation(x)) / h;
+        }
+    }
+}
